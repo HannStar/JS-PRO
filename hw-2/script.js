3dbe7c372ebe973bad7fdeff7444b5b97d7ae6f1
Gleb Kvarternikov <hannstar98@gmail.com>
2020-06-15 16:44:38 +0300

    create hw-2

---
+++ hw-2/script.js
@@ -0,0 +1,42 @@
+function createCounter(initialValue) {
+  return {
+    objValue: initialValue,
+    increment(value = 1) {
+      this.objValue += value;
+      console.log(this.objValue);
+      return this;
+    },
+
+    decrement(value = 1) {
+      this.objValue -= value;
+      console.log(this.objValue);
+      return this;
+    },
+
+    showValue() {
+      console.log(this.objValue);
+      return this;
+    },
+
+    discard() {
+      this.objValue = 0;
+      console.log(this.objValue);
+      return this;
+    },
+  };
+}
+
+const counter = createCounter(0);
+counter.increment(); // 1
+counter.increment(); // 2
+counter.increment(3); // 5
+counter.showValue(); // 5
+counter.decrement(); // 4
+counter.decrement(10); // -6
+counter.discard(); // 0
+
+counter.increment().increment(20).decrement(3).showValue();
+
+// module.exports = createCounter;
+
+console.log(counter);
